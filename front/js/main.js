6c5daf0ea761f4c860c12d22fffc14195b3639bd
jiro94 <sabe.jiro@gmail.com>
2020-03-05 17:53:08 +0900

    fix: front

--- front/js/main.js
+++ front/js/main.js
@@ -1,65 +1,95 @@
 var app = new Vue({
     el: '#app',
     data: {
-        selected: 1,
-        options: [
-            { text: '前方一致', value: 1 },
-            { text: '部分一致', value: 2 },
-            { text: '前方一致＋部分一致', value: 3 },
-            { text: 'Web', value: 4 }
-        ],
-        titles: null,
-        authors: null,
+        prefix: {
+            titles: null,
+            authors: null,
+        },
+        partial: {
+            titles: null,
+            authors: null,
+        },
+        prefixAndPartial: {
+            titles: null,
+            authors: null,
+        },
+        web: {
+            titles: null,
+            authors: null,
+        },
         keyword: '',
         message: '',
+        searchMethod: '',
+        limit: 20,
     },
     watch: {
         keyword: function (newKeyword, oldKeyword) {
-            this.debouncedGetAnswer()
+            this.debouncedGetSearchSuggestion()
         },
-        selected: function (newSelected, oldSelected) {
-            this.debouncedGetAnswer()
-        }
     },
     created: function () {
-        this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+        this.debouncedGetSearchSuggestion = _.debounce(this.getSearchSuggestion, 1000)
     },
     methods: {
-        getAnswer: function() {
-            const V5_PREFIX_REQUEST_URL = 'http://localhost:9000/v5_prefix/search/suggestion'
-            const V5_PARTIAL_REQUEST_URL = 'http://localhost:9000/v5_partial/search/suggestion'
-            const V5_PREFIX_PARTIAL_REQUEST_URL = 'http://localhost:9000/v5_prefix_partial/search/suggestion'
-            const WEB_REQUEST_URL = 'http://localhost:9000/web/search/suggestion'
+        getSearchSuggestion: function() {
+            const V5_PREFIX_REQUEST_URL = 'https://www.dev.nagisagames.jp/v5_prefix/search/suggestion'
+            const V5_PARTIAL_REQUEST_URL = 'https://www.dev.nagisagames.jp/v5_partial/search/suggestion'
+            const V5_PREFIX_PARTIAL_REQUEST_URL = 'https://www.dev.nagisagames.jp/v5_prefix_partial/search/suggestion'
+            const WEB_REQUEST_URL = 'https://www.dev.nagisagames.jp/web/search/suggestion'
+
             if(this.keyword === '') {
-                this.titles = null
-                this.authors = null
+                this.prefix.titles = null
+                this.prefix.authors = null
+                this.partial.titles = null
+                this.partial.authors = null
+                this.prefixAndPartial.titles = null
+                this.prefixAndPartial.authors = null
+                this.web.titles = null
+                this.web.authors = null
+                this.searchMethod = ''
                 return
             }
             this.message = 'Loading...'
             var vm = this
-            var params = { keyword: this.keyword, limit: 1000 }
-            var url = ''
-            switch (this.selected) {
-                case 1:
-                    url = V5_PREFIX_REQUEST_URL
-                    break
-                case 2:
-                    url = V5_PARTIAL_REQUEST_URL
-                    break
-                case 3:
-                    url = V5_PREFIX_PARTIAL_REQUEST_URL
-                    break
-                case 4:
-                    url = WEB_REQUEST_URL
-                    break
+            var params = { keyword: this.keyword, limit: this.limit }
+            this.getPrefixSearchSuggestion(V5_PREFIX_REQUEST_URL, vm, params)
+            this.getPartialSearchSuggestion(V5_PARTIAL_REQUEST_URL, vm, params)
+            this.getPrefixPartialSearchSuggestion(V5_PREFIX_PARTIAL_REQUEST_URL, vm, params)
+            this.getWebSearchSuggestion(WEB_REQUEST_URL, vm, params)
+            this.searchMethod = 'サジェスト検索結果：'
+        },
+        getSearch: function() {
+            const V5_PREFIX_REQUEST_URL = 'https://www.dev.nagisagames.jp/v5_prefix/search'
+            const V5_PARTIAL_REQUEST_URL = 'https://www.dev.nagisagames.jp/v5_partial/search'
+            const V5_PREFIX_PARTIAL_REQUEST_URL = 'https://www.dev.nagisagames.jp/v5_prefix_partial/search'
+            const WEB_REQUEST_URL = 'https://www.dev.nagisagames.jp/web/search'
 
+            if(this.keyword === '') {
+                this.prefix.titles = null
+                this.prefix.authors = null
+                this.partial.titles = null
+                this.partial.authors = null
+                this.prefixAndPartial.titles = null
+                this.prefixAndPartial.authors = null
+                this.web.titles = null
+                this.web.authors = null
+                this.searchMethod = ''
+                return
             }
+            this.message = 'Loading...'
+            var vm = this
+            var params = { keyword: this.keyword, limit: this.limit }
+            this.getPrefixSearch(V5_PREFIX_REQUEST_URL, vm, params)
+            this.getPartialSearch(V5_PARTIAL_REQUEST_URL, vm, params)
+            this.getPrefixPartialSearch(V5_PREFIX_PARTIAL_REQUEST_URL, vm, params)
+            this.getWebSearch(WEB_REQUEST_URL, vm, params)
+            this.searchMethod = 'キーワード検索結果：'
+        },
+        getPrefixSearchSuggestion: function(url, vm, params) {
             axios.get(url, { params })
               .then(function(response){
-                  vm.authors = response.data.author.items
-                  vm.titles = response.data.product.items
-                  console.log(vm.titles)
-                  console.log(vm.authors)
+                  vm.prefix.titles = response.data.product.items
+                  vm.prefix.authors = response.data.author.items
               })
               .catch(function (error) {
                   vm.message = 'Error!' + error
@@ -67,6 +97,97 @@ var app = new Vue({
               .finally(function () {
                   vm.message = ''
               })
-        }
+        },
+        getPartialSearchSuggestion: function(url, vm, params) {
+            axios.get(url, { params })
+              .then(function(response){
+                  vm.partial.titles = response.data.product.items
+                  vm.partial.authors = response.data.author.items
+              })
+              .catch(function (error) {
+                  vm.message = 'Error!' + error
+              })
+              .finally(function () {
+                  vm.message = ''
+              })
+        },
+        getPrefixPartialSearchSuggestion: function(url, vm, params) {
+            axios.get(url, { params })
+              .then(function(response){
+                  vm.prefixAndPartial.titles = response.data.product.items
+                  vm.prefixAndPartial.authors = response.data.author.items
+              })
+              .catch(function (error) {
+                  vm.message = 'Error!' + error
+              })
+              .finally(function () {
+                  vm.message = ''
+              })
+        },
+        getWebSearchSuggestion: function(url, vm, params) {
+            axios.get(url, { params })
+              .then(function(response){
+                  vm.web.titles = response.data.product.items
+                  vm.web.authors = response.data.author.items
+              })
+              .catch(function (error) {
+                  vm.message = 'Error!' + error
+              })
+              .finally(function () {
+                  vm.message = ''
+              })
+        },
+        getPrefixSearch: function(url, vm, params) {
+            axios.get(url, { params })
+              .then(function(response){
+                  vm.prefix.titles = response.data.items
+                  vm.prefix.authors = null
+              })
+              .catch(function (error) {
+                  vm.message = 'Error!' + error
+              })
+              .finally(function () {
+                  vm.message = ''
+              })
+        },
+        getPartialSearch: function(url, vm, params) {
+            axios.get(url, { params })
+              .then(function(response){
+                  vm.partial.titles = response.data.items
+                  vm.partial.authors = null
+              })
+              .catch(function (error) {
+                  vm.message = 'Error!' + error
+              })
+              .finally(function () {
+                  vm.message = ''
+              })
+        },
+        getPrefixPartialSearch: function(url, vm, params) {
+            axios.get(url, { params })
+              .then(function(response){
+                  vm.prefixAndPartial.titles = response.data.items
+                  vm.prefixAndPartial.authors = null
+              })
+              .catch(function (error) {
+                  vm.message = 'Error!' + error
+              })
+              .finally(function () {
+                  vm.message = ''
+              })
+        },
+        getWebSearch: function(url, vm, params) {
+            axios.get(url, { params })
+              .then(function(response){
+                  vm.web.titles = response.data.items
+                  vm.web.authors = null
+              })
+              .catch(function (error) {
+                  vm.message = 'Error!' + error
+              })
+              .finally(function () {
+                  vm.message = ''
+              })
+        },
     }
 })
